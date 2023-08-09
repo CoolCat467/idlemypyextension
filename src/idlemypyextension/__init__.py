5bb5b6911622c2ca5876ae44a9b8f92264f96dd7
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2023-08-09 00:29:09 -0500

    v0.2.0

--- src/idlemypyextension/__init__.py
+++ src/idlemypyextension/__init__.py
@@ -7,9 +7,9 @@ from __future__ import annotations
 __title__ = "idlemypyextension"
 __author__ = "CoolCat467"
 __license__ = "GPLv3"
-__version__ = "1.1.0"
-__ver_major__ = 1
-__ver_minor__ = 0
+__version__ = "0.2.0"
+__ver_major__ = 0
+__ver_minor__ = 2
 __ver_patch__ = 0
 
 import json
