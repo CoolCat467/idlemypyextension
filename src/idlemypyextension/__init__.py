71895da559ffc903246955052bfa97f200aed5d6
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2023-11-12 11:03:27 -0600

    Version bump

--- src/idlemypyextension/__init__.py
+++ src/idlemypyextension/__init__.py
@@ -7,10 +7,10 @@ from __future__ import annotations
 __title__ = "idlemypyextension"
 __author__ = "CoolCat467"
 __license__ = "GPLv3"
-__version__ = "0.2.4"
+__version__ = "0.2.5"
 __ver_major__ = 0
 __ver_minor__ = 2
-__ver_patch__ = 4
+__ver_patch__ = 5
 
 import json
 import math
