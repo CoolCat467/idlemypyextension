1799d7d71cfac66f1361feb555054da424150bba
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2024-02-24 14:55:46 -0600

    Bump to version `1.0.0`

--- src/idlemypyextension/__init__.py
+++ src/idlemypyextension/__init__.py
@@ -22,7 +22,7 @@ from __future__ import annotations
 __title__ = "idlemypyextension"
 __author__ = "CoolCat467"
 __license__ = "GNU General Public License Version 3"
-__version__ = "0.3.0"
+__version__ = "1.0.0"
 
 
 from idlemypyextension import utils
